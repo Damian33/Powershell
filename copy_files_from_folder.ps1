5a857ba732dee8a623d84a94f6f627ab4092704d
Damian33 <damian.cavaille@gmail.com>
2017-05-11 12:03:07 +0200

    copy_files_from_folder.ps1
    Script powershell de copie d'une arborescence de dossier

---
+++ copy_files_from_folder.ps1
@@ -0,0 +1,2 @@
+Write-Host "Copie 02_AS_SIDE vers Package"
+cp -r "C:\PREINT\appli\projects\PINT1\was70-01-std\Stdl02\installedApps\noeyyxthStdl02PINT1\DTR_PS.ear\edf.war\*" "C:\PREINT_PARAM\WAS\DTR_PS.ear\edf.war"
