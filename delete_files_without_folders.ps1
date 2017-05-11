dd015483b22ce08970a91838fbf057aeec7da731
Damian33 <damian.cavaille@gmail.com>
2017-05-11 12:01:04 +0200

    delete_files_without_folders.ps1
    Script powershell permettant de supprimer tous les fichiers contenus dans une arborescence de dossier

---
+++ delete_files_without_folders.ps1
@@ -0,0 +1,10 @@
+<#$files=Get-ChildItem -path "C:\TMA_THAIP\Application\DOCTHER\Integration SETH V4.1\v4.1_full"#>
+$files=Get-ChildItem -path "C:\TMA_THAIP\Application\DOCTHER\Integration SETH V4.1\v4.1_full" -Recurse
+
+Foreach ($element in $files) {
+	$type=$element.GetType()
+	if ($type.Name -Match "FileInfo") {
+		Write-Host "Suppression du fichier :"$element.FullName
+		Remove-Item $element.FullName
+	}
+}
